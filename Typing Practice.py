c89b780c78eacafa9aa35b2861a346a163fede42
hansdiamante <hans.diamante.cics@ust.edu.ph>
2023-03-10 00:34:10 +0800

    added qwer

--- Typing Practice.py
+++ Typing Practice.py
@@ -2,18 +2,20 @@ import random
 import msvcrt
 import os
 
-character_sheet = [['a', 's', 'd', 'f'],['j', 'k', 'l', ';']]
+character_sheet = [['a', 's', 'd', 'f'],['j', 'k', 'l', ';'],['q', 'w', 'e', 'r']]
 
 def asdf():
     typePracticeLoop('asdf')
 def jkl_sc():
     typePracticeLoop('jkl;')
-    
+def qwer():
+    typePracticeLoop('qwer')
 
 def typePracticeLoop(keys):
     x = ""
     if keys == 'asdf': chars = character_sheet[0]
     elif keys == 'jkl;': chars = character_sheet[1]
+    elif keys == 'qwer': chars = character_sheet[2]
 
     while True:
         msg = random.choices(chars)[0]
@@ -28,12 +30,14 @@ def typePracticeLoop(keys):
 while True:
     print("Touch Typing Practice.\nBeginner:\n\
  Type 'asdf' -- familiarizing yourself with asdf keys LH\n\
- Type 'jkl;' -- familiarizing yourself with jkl; keys RH\
+ Type 'jkl;' -- familiarizing yourself with jkl; keys RH\n\
+ Type 'qwer' -- familiarizing yourself with asdf keys LH\
           ")
 
 
     choose = input()
     if choose == 'asdf':asdf()
     elif choose == 'jkl;':jkl_sc()
+    elif choose == 'qwer':qwer()
     else:continue
     
